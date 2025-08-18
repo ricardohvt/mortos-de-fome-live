1e2ca43987f0c7208923b75ef5a487db50d38537
Ricardo Henrique <ricardohenriquevt@gmail.com>
2025-08-18 15:45:54 -0300

    Update script.js

--- src/view/javascript/script.js
+++ src/view/javascript/script.js
@@ -1,8 +1,3 @@
-const searchInput = document.getElementById('searchInput');
-const searchButton = document.getElementById('searchButton');
-const recipeslist = document.getElementById('recipeslist');
-const modalContainer = document.getElementById('modalContainer');
-const recipesdetailsContent = document.getElementById('recipes-details-Content');
 // conversor
 
 function mostrarconversor() {
